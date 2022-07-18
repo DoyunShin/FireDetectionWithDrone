303efbc482989c2e09036019aa84a41dad2f1871
Seunghoon <89653142+Seunghoon0326@users.noreply.github.com>
2022-07-18 09:03:35 +0900

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
+params ={
+    "serviceKey": "yy0vBlt7HwfUrdgLyhGHmJDELHnoI6h6Xs+j74Z49X50gO9Q8vtqAlEu4vQU/N+vz0iychRQMFH6kka6Xhpt1g==",
+    "pageNo": "1",
+    "numOfRows": "1000",
+    "dataType": "json",
+    "base_date": "20220714",
+    "base_time": "1600",
+    "nx": "55",
+    "ny": "127"
+}
+
+response = requests.get(url, params=params)
+print(response.text)
